43b1cf068128b82c78a6fc9f20b3d631e6d1c484
brandon <brandon@bdotindustries.com>
2026-04-29 02:16:48 +0000

    Update app pages

---
+++ src/api/backendFunctions.js
@@ -0,0 +1,13 @@
+import { base44 } from './base44Client';
+
+export const analyzePersonality = base44.functions.analyzePersonality;
+export const generateSite = base44.functions.generateSite;
+export const writeEmail = base44.functions.writeEmail;
+export const scrapeLeadsV2 = base44.functions.scrapeLeadsV2;
+export const findLeads = base44.functions.findLeads;
+export const runPipeline = base44.functions.runPipeline;
+export const placesSearch = base44.functions.placesSearch;
+export const sendEmail = base44.functions.sendEmail;
+export const scrapeLeads = base44.functions.scrapeLeads;
+export const getData = base44.functions.getData;
+export const updateRecord = base44.functions.updateRecord;
---
+++ src/api/entities.js
@@ -0,0 +1,9 @@
+import { base44 } from './base44Client';
+
+export const Business = base44.entities.Business;
+export const GeneratedSite = base44.entities.GeneratedSite;
+export const EmailCampaign = base44.entities.EmailCampaign;
+export const Campaign = base44.entities.Campaign;
+
+// auth sdk:
+export const User = base44.auth;
